66173916debcca96c647c901506caa708b1d58e5
Rokasbarasa1 <>
2021-10-07 00:01:32 +0200

    Add support for analog pin

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,13 @@
 #![no_main]
 #![no_std]
 
-// use aux8::entry;
-// use cortex_m;
 use cortex_m::{iprintln, asm};
 use cortex_m_rt::entry;
-// use stm32f3_discovery;
-use stm32f3xx_hal;
-// use f3::hal::stm32f30x::{GPIOA, RCC};
-// use cortex_m_semihosting::hprintln;
-// #[allow(unused_imports)]
-// use aux6::{entry, iprint, iprintln};
 use panic_itm as _;
-use stm32f3xx_hal::{self as hal, pac, prelude::*};
+use stm32f3xx_hal::{self as hal, pac, prelude::*, adc, };
 
 #[entry]
 fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
-    let mut rcc = dp.RCC.constrain();
-    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
-
-    let mut pin = gpioa.pa8.into_pull_down_input(&mut gpioa.moder, &mut gpioa.pupdr);
-    let p = pac::ITM::take().unwrap();
-    let mut itm = p.ITM;
-    loop {
-        if pin.is_high().unwrap() {
-            iprintln!(&mut itm.stim[0], "Hello, world!");
-        }else{
-            iprintln!(&mut itm.stim[0], "NULL WORLD");
-        }
-        // led.toggle().unwrap();
-        asm::delay(8_000_000);
-    }
-    
-
-    // let mut temperature: i32 = -1;
-    // let mut light_level: i32 = -1;
-    // let mut humidity_level: u8 = 0;
-
     //FREE IO 
     // A 1-4, 8-10, 15
     // B 0-2, 4-5, 8-15
@@ -45,22 +15,45 @@ fn main() -> ! {
     // D 0-15
     // E 6-7
     // F 2,4,6,9,10
-    // Turn on GPIOA_8 port
-    // let p = stm32f3xx_hal
-    // stm32f3xx_hal::gpio::Gpioa
-    // let gpioa = GPIOA::ptr();
-    // let rcc = RCC::ptr();
-    // (*rcc).ahbenr.write(|w| w.iopaen().set_bit());
 
-    // (*gpioa).moder.write(|w| {
-    //     w.moder8().input()
-    // });
+    //Analog values 
+    // 0 -> 0 mV
+    // 2048 -> 1500 mV
+    // 4095 -> 3000 mV
 
-    // GPIOx_MODER - modify port
-    // GPIOx_OTYPER and GPIOx_OSPEEDR registers are used to select the output type (push-pull or open-drain) and speed
-    // asm::bkpt();
 
-    // loop {
+    let mut dp = pac::Peripherals::take().unwrap();
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+    let mut clocks = rcc.cfgr.freeze(&mut dp.FLASH.constrain().acr);
+    let mut stim = &mut cortex_m::Peripherals::take().unwrap().ITM.stim[0];
+    let mut adc1 = adc::Adc::adc1(
+        dp.ADC1,
+        &mut dp.ADC1_2,
+        &mut rcc.ahb,
+        adc::ClockMode::default(),
+        clocks,
+    );
 
-    // }
+    let mut pin0 = gpioa.pa1.into_analog(&mut gpioa.moder, &mut gpioa.pupdr);
+
+    // let mut temperature: i32 = -1;
+    // let mut light_level: i32 = -1;
+    // let mut humidity_level: u8 = 0;
+
+    loop {
+        let pin0_data: u16 = adc1.read(&mut pin0).expect("Error reading adc1.");
+        iprintln!(stim, "result {}", map_adc_light_percent(pin0_data) );
+        asm::delay(1_000_000);
+    }
+}
+
+// light sensor goes from 3700 adc to 4080
+// Lower value after map means the room is brighter
+fn map_adc_light_percent(data: u16) -> u16{
+
+    let difference: u16 = 4096 as u16 - data;
+    let result: u16 = 100 - (difference * 100 / 396);
+
+    return result;
 }
